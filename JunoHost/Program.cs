2e15891e601d8812fb7954f226f4a9608ad1ef99
Fike Rehman <frehman@ncm.com>
2019-08-21 16:43:11 -0500

    Add project files.

---
+++ JunoHost/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace JunoHost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+                        Host.CreateDefaultBuilder(args)
+                            .UseWindowsService()
+                            .ConfigureLogging((context, logger) =>
+                            {
+                                logger.AddLog4Net();
+                            })
+                            .ConfigureServices((hostContext, services) =>
+                            {
+                                services.AddHostedService<Worker>();
+                            });
+    }
+}
---
+++ JunoHost/Worker.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JunoHost
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public override Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Worker started at: {DateTime.Now}");
+
+            return base.StartAsync(cancellationToken);
+        }
+
+        public override Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation($"Worker stopped at: {DateTime.Now}");
+
+            return base.StopAsync(cancellationToken);
+        }
+
+    }
+}
